ad9bd7de222cf3ec38524f83e85c4771655201f4
Tony <antoniocantone96@googlemail.com>
2019-11-10 15:12:10 +0000

    Added more links in credits

--- candybot/candybot/commands/credits.py
+++ candybot/candybot/commands/credits.py
@@ -14,12 +14,15 @@ class CreditsCommand(Command):
 
     title = ":bow: CandyBot Credits"
     donate_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4MA3ZWKYSYNB6"
+    add_link = "https://discordapp.com/api/oauth2/authorize?client_id=409047597572030484&permissions=8224&scope=bot"
+    github = "https://github.com/axc450/CandyBot"
 
     async def _run(self):
         donators = database.get_donators()
         donators = [f"**{x}**" for x in donators]
         donators_str = "\n".join(donators)
         credits_str = ":candy: CandyBot Created By **Super#0010**\n" \
+                      f":computer: [Add CandyBot to your server!]({self.add_link}) | [Github]({self.github})\n" \
                       f":moneybag: Please consider donating [here]({self.donate_link}) to support CandyBot!\n" \
                       f":heart: CandyBot supported by:\n{donators_str}"
         await self.send(credits_str)
