eab5cadd5f38260a014a0038f05ba4842d07c591
Tony <antoniocantone96@googlemail.com>
2019-11-11 20:02:42 +0000

    Fixed test deprecation warning

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,14 +9,14 @@ class Utils(TestCase):
     def test_chance_value_to_percent(self):
         test_input = [Mock(chance=2), Mock(chance=3), Mock(chance=5)]
         expected = {test_input[0]: 20, test_input[1]: 30, test_input[2]: 50}
-        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
+        self.assertEqual(utils.chance_value_to_percent(test_input), expected)
 
     def test_chance_value_to_percent_all_zero(self):
         test_input = [Mock(chance=0), Mock(chance=0), Mock(chance=0)]
         expected = {test_input[0]: 0, test_input[1]: 0, test_input[2]: 0}
-        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
+        self.assertEqual(utils.chance_value_to_percent(test_input), expected)
 
     def test_chance_value_to_percent_empty(self):
         test_input = []
         expected = {}
-        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
+        self.assertEqual(utils.chance_value_to_percent(test_input), expected)
