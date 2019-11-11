e9e5dabe7691a267fd85b420b506e4e946bd0990
Tony <antoniocantone96@googlemail.com>
2019-11-11 20:00:25 +0000

    Fixed bug with chance to percent conversion and added test

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from candybot import utils
+
+
+class Utils(TestCase):
+
+    def test_chance_value_to_percent(self):
+        test_input = [Mock(chance=2), Mock(chance=3), Mock(chance=5)]
+        expected = {test_input[0]: 20, test_input[1]: 30, test_input[2]: 50}
+        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
+
+    def test_chance_value_to_percent_all_zero(self):
+        test_input = [Mock(chance=0), Mock(chance=0), Mock(chance=0)]
+        expected = {test_input[0]: 0, test_input[1]: 0, test_input[2]: 0}
+        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
+
+    def test_chance_value_to_percent_empty(self):
+        test_input = []
+        expected = {}
+        self.assertEquals(utils.chance_value_to_percent(test_input), expected)
