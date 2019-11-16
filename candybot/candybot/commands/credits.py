d8b03f47fc65477fdd4513a9e59613fa8555d9d4
TaylahJayne <40306981+TaylahJayne@users.noreply.github.com>
2019-11-17 03:36:32 +1100

    Updated how donators are displayed

--- candybot/candybot/commands/credits.py
+++ candybot/candybot/commands/credits.py
@@ -18,7 +18,8 @@ class CreditsCommand(Command):
     async def _run(self):
         donators = database.get_donators()
         donators = [f"**{x}**" for x in donators]
-        donators_str = "\n".join(donators)
+        # TODO: Create 2 columns
+        donators_str = " ".join(donators)
         credits_str = ":candy: CandyBot Created By **Super#0010**\n" \
                       f":moneybag: Please consider donating [here]({self.donate_link}) to support CandyBot!\n" \
                       f":heart: CandyBot supported by:\n{donators_str}"
