721eedf1107853ea509dc4a1e0330f906018d8b0
Tony <antoniocantone96@googlemail.com>
2019-11-21 21:01:50 +0000

    Updated Credits command

--- candybot/candybot/commands/credits.py
+++ candybot/candybot/commands/credits.py
@@ -12,19 +12,17 @@ class CreditsCommand(Command):
     admin = False
     ignore = False
 
-    title = ":bow: CandyBot Credits"
+    title = ":heart: CandyBot Credits"
     donate_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4MA3ZWKYSYNB6"
     add_link = "https://discordapp.com/api/oauth2/authorize?client_id=409047597572030484&permissions=8224&scope=bot"
     github = "https://github.com/axc450/CandyBot"
 
     async def _run(self):
         donators = database.get_donators()
-        donators = [f"**{x}**" for x in donators]
-        # TODO: Create 2 columns
-        donators_str = " ".join(donators)
+        donators_list_1 = "\n".join(donators[:len(donators)//2])
+        donators_list_2 = "\n".join(donators[len(donators)//2:])
         credits_str = ":candy: CandyBot Created By **Super#0010**\n" \
                       f":computer: [Add CandyBot to your server!]({self.add_link}) | [Github]({self.github})\n" \
-                      f":moneybag: Please consider donating [here]({self.donate_link}) to support CandyBot!\n" \
-                      f":heart: CandyBot supported by:\n{donators_str}"
-        await self.send(credits_str)
+                      f":moneybag: Please consider donating [here]({self.donate_link}) to support CandyBot!"
+        await self.send(credits_str, fields=[("Donators", donators_list_2, True), ("Donators", donators_list_1, True)])
 
