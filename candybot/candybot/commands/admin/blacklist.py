6654de2a56498880344683e18600526969f94c4e
mkalluri21 <32691529+mkalluri21@users.noreply.github.com>
2019-11-22 11:53:15 -0800

    added show blacklist
    using the blacklist command with no arguments should pull up a list of users (tags) which are currently blacklisted.

--- candybot/candybot/commands/admin/blacklist.py
+++ candybot/candybot/commands/admin/blacklist.py
@@ -4,18 +4,24 @@ from candybot.commands.framework import AdminCommand, ArgumentSpec, UserArgument
 
 class BlacklistCommand(AdminCommand):
     name = "blacklist"
-    help = "Blacklists a user from interacting with CandyBot."
+    help = "Shows current blacklist or blacklists a user from interacting with CandyBot."
     aliases = ["bl"]
-    examples = ["@User", "User#1234", "123456789"]
+    examples = ["", "@User", "User#1234", "123456789"]
     argument_spec = ArgumentSpec([UserArgument], False)
     clean = True
     ignore = False
 
+    title = ":lock: CandyBot Blacklist"
+    
     async def _run(self):
-        blacklist = database.get_blacklist(self.message.guild.id)
-        if self.user.id in blacklist:
-            database.set_blacklist(self.message.guild.id, self.user.id, remove=True)
-            await self.send(f"{self.user.mention} was removed from the CandyBot blacklist")
+        blacklist = database.get_blacklist()
+        if self.user is None:
+            # How to get user mentions from DB?
+            await self.send("\n".join(blacklist))
         else:
-            database.set_blacklist(self.message.guild.id, self.user.id, remove=False)
-            await self.send(f"{self.user.mention} was added to the CandyBot blacklist")
+            if self.user.id in blacklist:
+                database.set_blacklist(self.message.guild.id, self.user.id, remove=True)
+                await self.send(f"{self.user.mention} was removed from the CandyBot blacklist")
+            else:
+                database.set_blacklist(self.message.guild.id, self.user.id, remove=False)
+                await self.send(f"{self.user.mention} was added to the CandyBot blacklist")
