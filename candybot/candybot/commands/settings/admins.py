0e25749eb5b49772a0596635e5b891ee59c4b3f6
Tony <antoniocantone96@googlemail.com>
2019-11-23 14:23:25 +0000

    Admins command can now be used without an argument (shows admins)

--- candybot/candybot/commands/settings/admins.py
+++ candybot/candybot/commands/settings/admins.py
@@ -1,22 +1,27 @@
-from candybot.interface import database
+from candybot.interface import database, converters
 from candybot.commands.framework import SettingsCommand, ArgumentSpec, UserArgument
 
 
 class AdminsCommand(SettingsCommand):
     name = "admins"
-    help = "Adds or removes CandyBot admins"
+    help = "Adds or removes CandyBot admins or shows admins"
     aliases = []
-    examples = []
-    argument_spec = ArgumentSpec([UserArgument], False)
+    examples = [""]
+    argument_spec = ArgumentSpec([UserArgument], True)
     clean = True
     ignore = False
 
     # TODO: Ask for confirmation
     async def _run(self):
         admins = database.get_admins(self.message.guild.id)
-        if self.user.id in admins:
-            database.set_admin(self.message.guild.id, self.user.id, remove=True)
-            await self.send(f"{self.user.mention} was removed as a CandyBot admin")
+        if self.user is None:
+            self.title = ":lifter: CandyBot Admins"
+            admins = [(await converters.to_user(str(x), self.message.guild)).mention for x in admins]
+            await self.send("\n".join(admins) if admins else "All")
         else:
-            database.set_admin(self.message.guild.id, self.user.id, remove=False)
-            await self.send(f"{self.user.mention} was added as a CandyBot admin")
+            if self.user.id in admins:
+                database.set_admin(self.message.guild.id, self.user.id, remove=True)
+                await self.send(f"{self.user.mention} was removed as a CandyBot admin")
+            else:
+                database.set_admin(self.message.guild.id, self.user.id, remove=False)
+                await self.send(f"{self.user.mention} was added as a CandyBot admin")
